4cdbedf5676d2b9e2d615cb2ccd4052bf175a645
jkerner003 <kerner.jo@gmail.com>
2023-08-17 15:55:40 +0200

    init

--- src/agent/retrieval_agent.py
+++ src/agent/retrieval_agent.py
@@ -9,6 +9,7 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.chains import create_qa_with_sources_chain
+import json
 
 class RetrievalAgent:
     def __init__(self):
@@ -64,6 +65,21 @@ class RetrievalAgent:
         query += f"User question: {input_question}, Retrieval agent Answer: "
         return query
 
+    def parse_answer(self, answer: str):
+        '''
+        Will parse answer
+        :param answer:
+        :return:
+        '''
+        #remove /n and /t from json string
+        answer = answer.replace('\n','')
+        #convert string to json
+        answer = json.loads(answer)
+        source = str(answer['sources'][0]).replace('docs\\','')
+        return answer['answer'], source
+
+
+
     def chat(self, question: Question) -> Answer:
 
         query = self.get_full_question(input_question=question.question,
@@ -71,14 +87,13 @@ class RetrievalAgent:
 
         answer = self.qa_agent({"query": query})
 
-        print('---------')
-        print(answer)
-        print('---------')
+
 
         answer_text = answer['result']
-        source = answer['sources']
+        answer, source = self.parse_answer(answer_text)
+
 
         question.context.append((question.question,answer_text))
-        return Answer(answer=answer_text,
+        return Answer(answer=answer,
                       past_conversations=question.context,
                       source=source)
