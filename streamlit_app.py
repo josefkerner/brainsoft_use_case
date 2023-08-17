4b9ba9602858a7656ae36babccb89fd85bd99be1
jkerner003 <kerner.jo@gmail.com>
2023-08-17 17:36:42 +0200

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,16 +16,22 @@ class UI:
         if "messages" not in st.session_state:
             st.session_state["messages"] = [{"role": "assistant", "content": "I can help to answer questions about GEN AI documentation. What is your answer?"}]
 
-        for msg in st.session_state.messages:
-            st.chat_message(msg["role"]).write(msg["content"])
+        # Display chat messages
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
 
         if prompt := st.chat_input():
             q = Question(question=prompt,
                          context=st.session_state['context']
                          )
+            st.session_state['messages'].append({"role": "user", "content": prompt})
+
             answer:Answer = self.controller.chat(q)
             st.session_state['context'] = answer.past_conversations
             full_answer = f"{answer.answer} Retrieved from: {answer.source}"
+            st.session_state['messages'].append({"role": "assistant", "content": full_answer})
+
             st.chat_message("assistant").write(full_answer)
 
 
